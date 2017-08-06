95308eb6fc2c5d92c05fa18e306033acccb83aef
Gaurav Talwar <megaurav2002@gmail.com>
2017-08-06 16:46:21 +1000

    can now read typed message for slack

--- src/background.js
+++ src/background.js
@@ -1,3 +1,11 @@
-chrome.browserAction.onClicked.addListener(function () {
-    alert("on click handler");
+// background.js
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // Send a message to the active tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var activeTab = tabs[0];
+        alert("background.js clicked browser action");
+        chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
+    });
 });
--- src/content.js
+++ src/content.js
@@ -1,10 +1,35 @@
 // content.js
 
+// domainName: messageSelector
+var selectorList = {
+    'slack.com': 'div.ql-editor p'
+}
+
+
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        alert(request.message);
         if (request.message === "clicked_browser_action") {
-            alert("in content file");
+            var selector;
+            var message;
+
+            for (var key in selectorList) {
+                if (document.URL.includes(key)) {
+                    selector = selectorList[key];
+                }
+            }
+            alert(selector);
+
+            var node = document.querySelector(selector);
+
+            if (!node) {
+                return;
+            }
+            else if (node.nodeName.toLowerCase() === 'p') {
+                message = node.innerText;
+            }
+
+            alert(message);
+
         }
     }
 );
