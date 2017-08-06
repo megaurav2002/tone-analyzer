9d438cf796228d099b03bc4fac5699e55977bf9e
Gaurav Talwar <megaurav2002@gmail.com>
2017-08-06 19:47:03 +1000

    cleanup

--- src/background.js
+++ src/background.js
@@ -5,7 +5,6 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     // Send a message to the active tab
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var activeTab = tabs[0];
-        alert("background.js clicked browser action");
         chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
     });
 });
